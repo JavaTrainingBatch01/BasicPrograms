848df5bf3027eef094dd026a322e403c0834061e
paparao <paparao.mandala1@gmail.com>
2023-11-04 17:58:21 +0530

    abcd

--- src/amstrongNum.java
+++ src/amstrongNum.java
@@ -17,10 +17,10 @@ public class amstrongNum {
 
 		}
 		if(sum == temp) {
-			System.out.println("Armstrong number");
+			System.out.println("It is a Armstrong number");
 		}
 		else {
-			System.out.println("Not a Armstrong number");
+			System.out.println("It is Not a Armstrong number");
 		}
 		System.out.println(sum);
 	}
